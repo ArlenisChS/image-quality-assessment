6c86c377a79fcf1bd5ba5b98baaef4622d6c6aaa
Arlenis <arlenis.ching@electronicid.eu>
2021-11-09 19:30:13 +0100

    Adaptaciones para DNI

---
+++ run.sh
@@ -0,0 +1,4 @@
+sudo ./train-local \
+--config-file $(pwd)/models/MobileNet/config_technical_cpu_iq.json \
+--samples-file $(pwd)/data/ImageQuality/document_quality_train.json \
+--image-dir /home/arlenis/datasets/ImageQuality/
