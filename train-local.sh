b1f657930876ddade1af69ed65c90e0c3524e92d
Arlenis <arlenis.ching@electronicid.eu>
2021-11-10 19:26:44 +0100

    Fixing train-local weights if exist

--- train-local.sh
+++ train-local.sh
@@ -1,4 +1,5 @@
 sudo ./train-local \
 --config-file $(pwd)/models/MobileNet/config_technical_cpu_iq.json \
 --samples-file $(pwd)/data/ImageQuality/document_quality_train.json \
---image-dir /home/arlenis/datasets/ImageQuality/
+--image-dir /home/arlenis/datasets/ImageQuality/ \
+--weights-dir models/MobileNet/
